6f8e5a68cc888fedd92dcd60b162e5d6430c3f39
Dariusz Bosko <dariuszbosko95@gmail.com>
2023-04-01 16:25:44 +0200

    Revert "Initial"
    This reverts commit b341d43ce18d178524e49d8d5ad1580faa264ba2.

--- ClientApp/src/pages/Home.tsx
+++
@@ -1,7 +0,0 @@
-import React, { useEffect, useState } from 'react'
-
-const Home = (props: { isAuthenticated: boolean }) => {
-    return <div>{props.isAuthenticated ? 'You are logged in' : 'You are not logged in'}</div>
-}
-
-export default Home
--- Dtos/RegisterDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BetApp.Dtos
-{
-	public class RegisterDto
-	{
-		public string Name { get; set; }
-		public string Email { get; set; }
-		public string Password { get; set; }
-	}
-}
